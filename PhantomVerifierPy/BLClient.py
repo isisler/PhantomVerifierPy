c7652458624bd3a41ceca561b75b97a6699ecc0d
S1ngular1ty <cole28392@gmail.com>
2016-07-31 16:12:33 -0500

    Added some error handling and ability to check more players in a loop

--- PhantomVerifierPy/BLClient.py
+++ PhantomVerifierPy/BLClient.py
@@ -2,7 +2,10 @@ from urllib.request import Request, urlopen
 
 class BClient:    
     def fetchURL(self, url):
-        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-        response = urlopen(req).read()
-        urlstring = response.decode('utf-8')
-        return urlstring
+        try:
+            req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})                  
+            response = urlopen(req).read()
+            urlstring = response.decode('utf-8')            
+            return urlstring
+        except Exception as e:
+            raise            
